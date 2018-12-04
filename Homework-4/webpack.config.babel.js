43a16b5f077e5d0fef22acf1c148ab53a92feb3b
Volha Kunskaya <volha_kunskaya@epam.com>
2018-12-04 19:30:30 +0300

    homework-4 initial commit

---
+++ Homework-4/webpack.config.babel.js
@@ -0,0 +1,62 @@
+import path from 'path';
+import HtmlWebpackPlugin from 'html-webpack-plugin';
+import MiniCssExtractPlugin from 'mini-css-extract-plugin';
+
+export default {
+  entry: {
+    main: [
+      'babel-polyfill',
+      path.resolve(__dirname, 'css/style.scss'),
+      path.resolve(__dirname, 'css/loader.scss'),
+      path.resolve(__dirname, 'css/error.scss'),
+      path.resolve(__dirname, 'js/main.js'),
+      path.resolve(__dirname, 'js/file.json')
+    ]
+  },
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: '[name].bundle.js',
+    chunkFilename: '[name].bundle.js',
+    publicPath: ''
+  },
+  resolveLoader: {
+    modules: ['node_modules', path.resolve(__dirname, 'loaders')]
+  },
+  module: {
+    rules: [
+      {
+        test: /\.js$/,
+        exclude: /(node_modules)/,
+        use: 'babel-loader'
+      },
+      {
+        test: /\.(s*)css$/,
+        exclude: /node_modules/, 
+        use: [MiniCssExtractPlugin.loader, 'css-loader','sass-loader']
+      },
+      {
+        test: /\.json$/,
+        exclude: /node_modules/,
+        use: 'jsonLoader'
+      }
+    ]
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      filename: path.resolve(__dirname, 'dist/index.html'),
+      template: path.resolve(__dirname, 'index.html'),
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].css"
+    })
+  ],
+  watch: true,
+  devtool: 'source-map',
+  devServer: {
+    port: 9000,
+    compress: true,
+    writeToDisk: true,
+    contentBase: path.resolve(__dirname, 'dist'),
+    publicPath: '/'
+  }
+};
